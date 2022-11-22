b9edb20e71b170cb9da98166ccf56062376f16a8
murad-aliyev <98873214+murad-aliyev@users.noreply.github.com>
2022-11-22 14:40:24 +0400

    Add files via upload

---
+++ src/components/header/App.jsx
@@ -0,0 +1,11 @@
+import "./app.css"
+import Header2 from "./input"
+
+const App = () => {
+  return (
+    <div className='app'><h1>Now I can create component</h1>
+    <Header2/></div>
+  )
+}
+
+export default App
---
+++ src/components/header/input.jsx
@@ -0,0 +1,9 @@
+import React,{Component} from 'react'
+
+class Header2 extends Component {
+  render(){
+    return(<><p>Welcome to React!</p></>)
+  }
+}
+
+export default Header2
